f88002ea61dc755c91c603bc676c8d926c252f70
James Schumann <jschumann@gmail.com>
2017-04-28 07:34:14 -0400

    rip out footer info

--- footer.php
+++ footer.php
@@ -14,11 +14,6 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'inside-voice' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'inside-voice' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'inside-voice' ), 'inside-voice', '<a href="https://automattic.com/" rel="designer">WRDSB</a>' ); ?>
-		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
